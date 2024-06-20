da108df9188eaccce4061bffd8676d2a839bc83a
gauravmandall <mandalgaurav550@gmail.com>
2024-06-20 14:59:09 +0530

    feat: added web app -> setup with shared packages -> added global styles -> added layout -> added not found page -> added tailwind config -> updated package.json

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import { ContextMenu } from "@repo/ui/components/ui/context-menu";
+export default function NotFound() {
+
+  return (
+    <>
+      <div className="pointer-events-none relative -mt-[56px] flex h-screen flex-col items-center justify-center gap-8 overflow-hidden [&>*]:pointer-events-auto">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="mb-4 font-mono text-4xl font-bold leading-tight opacity-60 md:text-9xl">
+            404
+          </h1>
+          <p className="px-6 text-center font-mono text-base md:px-0 md:text-xl">
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
