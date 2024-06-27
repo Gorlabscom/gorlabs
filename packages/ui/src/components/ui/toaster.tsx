3105d93ad4b480f63105fe04a41190bb4298d9e3
gauravmandall <mandalgaurav550@gmail.com>
2024-06-28 02:32:26 +0530

    feat(ui): added components and fix known bugs - added logo - added icons - added types - added theme provider and theme toggle button - updated package.json and tsconfig.json - updated button, toast, and toaster components

--- packages/ui/src/components/ui/toaster.tsx
+++ packages/ui/src/components/ui/toaster.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   Toast,
@@ -7,29 +7,42 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@ui/components/ui/toast"
-import { useToast } from "@ui/components/ui/use-toast"
+} from "@repo/ui/components/ui/toast"
+import { useToast } from "./use-toast"
+import { AlertTriangle, CheckCircle2, InfoIcon } from "@repo/ui/icons";
+
+const ICONS = {
+  default: <InfoIcon fill="#8B5CF6" color="#fff" size={32} />,
+  success: <CheckCircle2 fill="#16A34A" color="#fff" size={32} />,
+  destructive: <AlertTriangle fill="#DC1C1C" color="#fff" size={32} />,
+};
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function ({ id, title, description, action, variant, ...props }) {
+        const Icon = variant ? ICONS[variant] : null;
+
         return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
+          <Toast key={id} {...props} variant={variant}>
+            <div className="z-10">
+              {Icon && (
+                <div className="flex h-[50px] w-[50px] items-center justify-center rounded-full border-2 border-white bg-white shadow-[inset_3px_2px_2px_rgba(0,0,0,0.06)]">
+                  {Icon}
+                </div>
               )}
             </div>
-            {action}
-            <ToastClose />
+            <div className="grid gap-1">
+              {title ? <ToastTitle>{title}</ToastTitle> : null}
+              {description ? <ToastDescription>{description}</ToastDescription> : null}
+            </div>
+            <div className="ml-auto inline-flex">{action ? action : <ToastClose />}</div>
           </Toast>
-        )
+        );
       })}
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
