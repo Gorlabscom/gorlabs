9f5cee173dbf16793f832ed566c153123369417e
gauravmandall <mandalgaurav550@gmail.com>
2024-07-09 02:51:33 +0530

    setup ESLint for Next.js * reconfigured .eslintrc.json to .eslintrc.js * updated package.json to reflect changes * fix: updated eslint-config-next.js to include new rules * fix: dependencies for eslint-config-next.js * fix: affected files in apps/frontend and apps/web * updated turbo.json

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -5,11 +5,22 @@ const project = resolve(process.cwd(), "tsconfig.json");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [
-    "eslint:recommended",
+    // "eslint:latest",
     "prettier",
     require.resolve("@vercel/style-guide/eslint/next"),
     "turbo",
-  ],
+    '@vercel/style-guide/eslint/node',
+    '@vercel/style-guide/eslint/typescript',
+    '@vercel/style-guide/eslint/browser',
+    '@vercel/style-guide/eslint/react',
+    '@vercel/style-guide/eslint/next',
+    // Turborepo custom eslint configuration configures the following rules:
+    //  - https://github.com/vercel/turbo/blob/main/packages/eslint-plugin-turbo/docs/rules/no-undeclared-env-vars.md
+    'eslint-config-turbo',
+  ].map(require.resolve),
+  parserOptions: {
+    project,
+  },
   globals: {
     React: true,
     JSX: true,
@@ -30,6 +41,7 @@ module.exports = {
     // Ignore dotfiles
     ".*.js",
     "node_modules/",
+    "dist/"
   ],
   overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
 };
