4874da2216516d041617adaebc1b5d308602b596
Gaurav Mandal <mandalgaurav550@gmail.com>
2025-02-06 12:26:00 +0530

    build(web): removed instrumentation

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,3 +1,3 @@
-import { initializeSentry } from '@repo/observability/instrumentation';
+// import { initializeSentry } from '@repo/observability/instrumentation';
 
-export const register = initializeSentry();
+// export const register = initializeSentry();
